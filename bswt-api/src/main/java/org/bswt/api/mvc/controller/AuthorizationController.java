4eccefc3b44da5ef89b56af85316ca2a06cc2f79
thomas.holdren <thomas.holdren@cardinalhealth.com>
2016-10-24 11:32:28 -0400

    Making auth controller accept coors.

--- bswt-api/src/main/java/org/bswt/api/mvc/controller/AuthorizationController.java
+++ bswt-api/src/main/java/org/bswt/api/mvc/controller/AuthorizationController.java
@@ -13,6 +13,7 @@ import javax.servlet.ServletException;
 import org.apache.commons.lang3.StringUtils;
 import org.bswt.api.SubjectsConfiguration;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/rest/auth")
 public class AuthorizationController
